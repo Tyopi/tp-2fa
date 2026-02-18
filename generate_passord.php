c68289f6f62c61b6a6d1c39423c568db90d12446
lariviere <matheo.lariviere2111@gmail.com>
2026-02-18 14:57:10 +0100

    hashage

---
+++ generate_passord.php
@@ -0,0 +1,65 @@
+<?php
+
+
+/*  CONNEXION BDD
+ 
+$dbPath = 'tp-2fa.db';
+$db = new SQLite3($dbPath, SQLITE3_OPEN_READWRITE);
+
+*/
+
+
+$password = "Vivelephp!2026";
+
+$hasher = password_hash($password, PASSWORD_BCRYPT);
+
+echo $hasher;
+
+/*  CRECUPERATION DU PASSWORD 
+ 
+$result = $db->query('SELECT * FROM users');
+
+*/
+
+$result = [
+    'id' => 1,
+    'email' => 'user@test.fr',
+    'password' => 'Vivelephp!2026',
+    'secret_2fa' => '',
+    'twofa_enabled' => 0,
+];
+
+echo "</br>";
+echo $result['password'];
+echo "</br>";
+
+$hasher = password_hash($result['password'], PASSWORD_BCRYPT);
+echo $hasher;
+
+/*  changement en bdd DU PASSWORD 
+ 
+$result = $db->query('UPDATE users SET password = $hasher WHERE email = $result['email'];');
+
+*/
+
+$result['password'] = $hasher;
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
